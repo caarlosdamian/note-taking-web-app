71b5ef93fce0a823a6eb4c3facaed43069fa3e6b
caarlosdamian <dapecarlos@gmail.com>
2025-08-04 19:36:28 -0600

    feat(styles): added some styles and clean app

---
+++ src/context/fontContext.tsx
@@ -0,0 +1,35 @@
+'use client';
+import { Inter, Noto_Serif, Source_Code_Pro } from 'next/font/google';
+import { createContext, PropsWithChildren, useState } from 'react';
+
+  const inter = Inter({
+    variable: '--font-inter',
+    subsets: ['latin'],
+  });
+
+  const noto = Noto_Serif({
+    variable: '--font-noto',
+    subsets: ['latin'],
+  });
+
+  const codePro = Source_Code_Pro({
+    variable: '--font-code-pro',
+    subsets: ['latin'],
+  });
+const fontContext = createContext({});
+
+export const FontContextProvider = ({ children }: PropsWithChildren) => {
+
+  const [font, setFont] = useState(codePro.className);
+
+  return (
+    <fontContext.Provider
+      value={{
+        font,
+        setFont,
+      }}
+    >
+      <main className={font}>{children}</main>
+    </fontContext.Provider>
+  );
+};
